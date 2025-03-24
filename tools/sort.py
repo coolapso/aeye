e89347baa3304255e86a891923f8eff8519b7176
Carlos Colaço <coolapso@coolapso.tech>
2025-03-24 22:23:28 +0100

    feat(tools/sort): Exclude
    Add keypress for completely excluding files from the data set,
    Excluded files will be moved to excluded directory and will not be shown again

--- tools/sort.py
+++ tools/sort.py
@@ -11,17 +11,20 @@ RESIZED_DIMENSION = (128, 128)
 dirs = {
         "no": "00-not-detected",
         "yes": "01-detected",
+        "excluded": "excluded",
     }
 
 texts = {
         "close": "[esc]: Close Window",
         "yes": "[y] Valid image",
         "no": "[n]: Invalid image",
+        "exclude": "[i]: ignore/exclude",
     }
 
 aurora_frames = []
 no_aurora_frames = []
 skipped_frames = []
+excluded_frames = []
 
 
 def review_frames(dataset_root_dir: str, frames_dir: str):
@@ -38,7 +41,8 @@ def review_frames(dataset_root_dir: str, frames_dir: str):
             img = cv2.putText(img, texts["close"], (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,255), 2)
             img = cv2.putText(img, texts["yes"], (0, 55), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,255), 2)
             img = cv2.putText(img, texts["no"], (0, 85), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,255), 2)
-            img = cv2.putText(img, remaining_frames_text, (0, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,255), 2)
+            img = cv2.putText(img, texts["exclude"], (0, 115), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,255), 2)
+            img = cv2.putText(img, remaining_frames_text, (0, 140), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,255), 2)
             cv2.imshow("frames", img)
             key = cv2.waitKey(0)
 
@@ -53,6 +57,9 @@ def review_frames(dataset_root_dir: str, frames_dir: str):
                 case 110:
                     print(f'''{frame} is not valid''')
                     no_aurora_frames.append(frame)
+                case 105:
+                    print(f'''{frame} excluded''')
+                    excluded_frames.append(frame)
                 case _:
                     print(f'''{frame} was skipped''')
                     skipped_frames.append(frame)
@@ -80,7 +87,9 @@ def move_frames(dataset_root_dir: str, frames_dir: str, frames: list, destinatio
         if not os.path.exists(resized_dest_dir):
             os.makedirs(resized_dest_dir)
 
-        resize_and_save(frame_path, resized_dest_dir)
+        if frames_dir != dirs["excluded"]:
+            resize_and_save(frame_path, resized_dest_dir)
+
         shutil.move(frame_path, os.path.join(dest_dir, frame))
 
 
@@ -122,6 +131,10 @@ def main():
     if len(no_aurora_frames) > 0:
         print(f'''Moving frames without aurora to {dirs["no"]}''')
         move_frames(dataset_root_dir, frames_dir, no_aurora_frames, dirs["no"])
+
+    if len(excluded_frames) > 0:
+        print(f'''Moving excluded frames to {dirs["excluded"]}''')
+        move_frames(dataset_root_dir, frames_dir, excluded_frames, dirs["excluded"])
 
     cv2.destroyAllWindows()
     if len(skipped_frames) > 0:
